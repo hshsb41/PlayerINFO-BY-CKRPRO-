13fc25404a144116d207fc02ce5763bf044d6407
hshsb41 <nansnanans0@gmail.com>
2026-07-12 10:20:48 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -48,13 +48,13 @@ async def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 def get_account_credentials(region: str) -> str:
     r = region.upper()
     if r == "ME":
-        return "uid=3825052753&password=2D99628D3083D88F0997093B5D3E65F5ED13321941FB7B3FCDFB207E203832BE"
+        return "uid=4337844075&password=CKR_19R9M__V0DZU"
     elif r == "BD":
-        return "uid=4316459091&password=9X_JUBAYER__TD5LO__Y76IB"
+        return "uid=4337844075&password=CKR_19R9M__V0DZU"
     elif r in {"BR", "US", "SAC", "ME"}:
-        return "uid=3788023112&password=5356B7495AC2AD04C0A483CF234D6E56FB29080AC2461DD51E0544F8D455CC24"
+        return "uid=4337844075&password=CKR_19R9M__V0DZU"
     else:
-        return "uid=3301239795&password=DD40EE772FCBD61409BB15033E3DE1B1C54EDA83B75DF0CDD24C34C7C8798475"
+        return "uid=4337844075&password=CKR_19R9M__V0DZU"
 
 # === Token Generation ===
 async def get_access_token(account: str):
@@ -180,7 +180,7 @@ async def get_account_info():
         return jsonify(formatted), 200
     
     except Exception as e:
-        return jsonify({"error": "Invalid UID or server error. Please try again.created by @jubayer_ahmed_34. join our telegram @Mr.ghost34"}), 500
+        return jsonify({"error": "Invalid UID or server error. Please try again.created by ckrpro YouTube : ckr unknown TikTok : ckr unknown"}), 500
 
 @app.route('/refresh', methods=['GET', 'POST'])
 def refresh_tokens_endpoint():
